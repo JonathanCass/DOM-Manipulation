875a9570b12e447f45ea78dee693138621b5e137
JonCass <JCass1980@gmail.com>
2017-03-14 14:30:35 -0700

    Sort of working looks ugly

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
+	var slideCheck = true;
 
+	if(slideCheck){
+		$( "#slider2" ).click(function() {
+			$(this).toggleClass("slide2")
+		})
+	}
+	$( "#slider3" ).click(function() {
+		$(this).toggleClass("slide3")
+		slideCheck = !slideCheck
+	})	
 })
